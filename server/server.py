28fe3d8f80a6b4eba360cb9198ec83c89e84bd92
Diastuti Utami <13514071@std.stei.itb.ac.id>
2017-04-28 22:27:51 +0700

    Fixing indentation, fix server script

--- server/server.py
+++ server/server.py
@@ -14,7 +14,7 @@ reset = False
 alive = True
 
 #Define server IP and Port
-server_ip = 127.0.0.1
+server_ip = '127.0.0.1'
 server_port = 10050
 
 #Class for life thread
@@ -25,31 +25,52 @@ class Life(threading.Thread):
         self.lifetime = 5
 
     def run(self):
-	    while(alive):
+        while(alive):
             while self.lifetime > 0:
-				if(reset)
+                if(reset):
 				    self.lifetime = 5
-				else
+                else:
 				    self.lifetime -= 0.1
-				self.event.wait(0.1)
-	
-	def stop(self):
+                if (self.lifetime) <= 0 :
+                    alive = False
+                self.event.wait(0.1)
+    def stop(self):
         self.event.set()
 
 #Create socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print 'Socket created'
 except socket.error, msg:
     print 'Failed to create socket'
-    sys.exit();
+    sys.exit()
+print 'Socket created'
 
 #Bind socket
+try:
+    s.bind((server_ip, server_port))
+except socket.error:
+    print 'Error binding'
+    sys.exit()
+print 'Bind socket completed'
 
 #Listen
+s.listen(10)
+print 'Listening...'
 
 #Start Thread
+life = Life()
+life.start()
 
 #Main
 while(alive):
     #Receive connection. Everytime a request received, timeout is reset
+    conn, addr = s.accept()
+    while 1:
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        data = conn.recv(1024)
+        if data:
+            print data
+            reset = True
+conn.close()
+s.close()
+life.stop()
